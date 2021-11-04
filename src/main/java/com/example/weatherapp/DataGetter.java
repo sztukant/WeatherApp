811c2d326aafea4eb78d685ce40139b7e016faee
sztukant <sztukant@gmail.com>
2021-11-04 14:50:47 +0100

    optimization

--- src/main/java/com/example/weatherapp/DataGetter.java
+++ src/main/java/com/example/weatherapp/DataGetter.java
@@ -5,13 +5,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataGetter {
-    private final JSONObject cities = new JSONObject("{\"Białystok\":\"bialystok\",\"Bielsko Biała\":\"bielskobiala\",\"Chojnice\":\"chojnice\",\"Częstochowa\":\"czestochowa\",\"Elbląg\":\"elblag\",\"Gdańsk\":\"gdansk\",\"Gorzów\":\"gorzow\",\"Hel\":\"hel\",\"Jelenia Góra\":\"jeleniagora\",\"Kalisz\":\"kalisz\",\"Kasprowy Wierch\":\"kasprowywierch\",\"Katowice\":\"katowice\",\"Kętrzyn\":\"ketrzyn\",\"Kielce\":\"kielce\",\"Kłodzko\":\"klodzko\",\"Koło\":\"kolo\",\"Kołobrzeg\":\"kolobrzeg\",\"Koszalin\":\"koszalin\",\"Kozienice\":\"kozienice\",\"Kraków\":\"krakow\",\"Krosno\":\"krosno\",\"Legnica\":\"legnica\",\"Lesko\":\"lesko\",\"Leszno\":\"leszno\",\"Lębork\":\"lebork\",\"Lublin\":\"lublin\",\"Łeba\":\"leba\",\"Łódź\":\"lodz\",\"Mikołajki\":\"mikolajki\",\"Mława\":\"mlawa\",\"Nowy Sącz\":\"nowysacz\",\"Olsztyn\":\"olsztyn\",\"Opole\":\"opole\",\"Ostrołęka\":\"ostroleka\",\"Piła\":\"pila\",\"Płock\":\"plock\",\"Poznań\":\"poznan\",\"Przemyśl\":\"przemysl\",\"Racibórz\":\"raciborz\",\"Resko\":\"resko\",\"Rzeszów\":\"rzeszow\",\"Sandomierz\":\"sandomierz\",\"Siedlce\":\"siedlce\",\"Słubice\":\"slubice\",\"Sulejów\":\"sulejow\",\"Suwałki\":\"suwalki\",\"Szczecin\":\"szczecin\",\"Szczecinek\":\"szczecinek\",\"Śnieżka\":\"sniezka\",\"Świnoujście\":\"swinoujscie\",\"Tarnów\":\"tarnow\",\"Terespol\":\"terespol\",\"Toruń\":\"torun\",\"Ustka\":\"ustka\",\"Warszawa\":\"warszawa\",\"Wieluń\":\"wielun\",\"Włodawa\":\"wlodawa\",\"Wrocław\":\"wroclaw\",\"Zakopane\":\"zakopane\",\"Zamość\":\"zamosc\",\"Zielona Góra\":\"zielonagora\"}");
+    public final JSONObject cities = new JSONObject("{\"Białystok\":\"bialystok\",\"Bielsko Biała\":\"bielskobiala\",\"Chojnice\":\"chojnice\",\"Częstochowa\":\"czestochowa\",\"Elbląg\":\"elblag\",\"Gdańsk\":\"gdansk\",\"Gorzów\":\"gorzow\",\"Hel\":\"hel\",\"Jelenia Góra\":\"jeleniagora\",\"Kalisz\":\"kalisz\",\"Kasprowy Wierch\":\"kasprowywierch\",\"Katowice\":\"katowice\",\"Kętrzyn\":\"ketrzyn\",\"Kielce\":\"kielce\",\"Kłodzko\":\"klodzko\",\"Koło\":\"kolo\",\"Kołobrzeg\":\"kolobrzeg\",\"Koszalin\":\"koszalin\",\"Kozienice\":\"kozienice\",\"Kraków\":\"krakow\",\"Krosno\":\"krosno\",\"Legnica\":\"legnica\",\"Lesko\":\"lesko\",\"Leszno\":\"leszno\",\"Lębork\":\"lebork\",\"Lublin\":\"lublin\",\"Łeba\":\"leba\",\"Łódź\":\"lodz\",\"Mikołajki\":\"mikolajki\",\"Mława\":\"mlawa\",\"Nowy Sącz\":\"nowysacz\",\"Olsztyn\":\"olsztyn\",\"Opole\":\"opole\",\"Ostrołęka\":\"ostroleka\",\"Piła\":\"pila\",\"Płock\":\"plock\",\"Poznań\":\"poznan\",\"Przemyśl\":\"przemysl\",\"Racibórz\":\"raciborz\",\"Resko\":\"resko\",\"Rzeszów\":\"rzeszow\",\"Sandomierz\":\"sandomierz\",\"Siedlce\":\"siedlce\",\"Słubice\":\"slubice\",\"Sulejów\":\"sulejow\",\"Suwałki\":\"suwalki\",\"Szczecin\":\"szczecin\",\"Szczecinek\":\"szczecinek\",\"Śnieżka\":\"sniezka\",\"Świnoujście\":\"swinoujscie\",\"Tarnów\":\"tarnow\",\"Terespol\":\"terespol\",\"Toruń\":\"torun\",\"Ustka\":\"ustka\",\"Warszawa\":\"warszawa\",\"Wieluń\":\"wielun\",\"Włodawa\":\"wlodawa\",\"Wrocław\":\"wroclaw\",\"Zakopane\":\"zakopane\",\"Zamość\":\"zamosc\",\"Zielona Góra\":\"zielonagora\"}");
     private JSONObject cityData;
-
+    public JSONObject city;
+    public String cityString;
+    public Map<String, String> cityList = new HashMap<>();
     void getData(String city) throws IOException {
         StringBuilder sb = new StringBuilder();
         sb.append("https://danepubliczne.imgw.pl/api/data/synop/station/").append(cities.getString(city)).append("/format/json");
@@ -25,5 +30,33 @@ public class DataGetter {
     }
     JSONObject showData(){
         return cityData;
+    }
+    void getCities() throws IOException {
+
+
+        cityString = IOUtils.toString(URI.create("https://danepubliczne.imgw.pl/api/data/synop/format/json"), StandardCharsets.UTF_8);
+        city = new JSONObject(cityString.substring(1, (cityString.length() - 2)));
+
+
+        for (int i = 0; i < cityString.length(); i++) {
+            StringBuilder s = new StringBuilder();
+            String str;
+            String key;
+            cityString = cityString.substring(cityString.indexOf("\"stacja\":\"") + 9, cityString.length() - 1);
+            if (cityString.indexOf("\"") == 0) {
+                cityString = cityString.substring(1, cityString.length() - 1);
+            }
+            s.append(cityString, 0, cityString.indexOf("\""));
+            key = s.toString();
+            if (s.toString().contains(" ")) {
+                s.deleteCharAt(s.indexOf(" "));
+            }
+            if (s.toString().contains("ą")) {
+                s.replace(s.toString().indexOf("ą"), s.toString().indexOf("ą"), "a");
+            }
+            str = s.toString().toLowerCase();
+            cityList.put(key, str);
+
+        }
     }
 }
--- src/main/java/com/example/weatherapp/HelloController.java
+++ src/main/java/com/example/weatherapp/HelloController.java
@@ -30,9 +30,9 @@ public class HelloController {
     public ComboBox cityChoice;
 
     @FXML
-    public void initialize(){
-        JSONObject city = new JSONObject("{\"Białystok\":\"bialystok\",\"Bielsko Biała\":\"bielskobiala\",\"Chojnice\":\"chojnice\",\"Częstochowa\":\"czestochowa\",\"Elbląg\":\"elblag\",\"Gdańsk\":\"gdansk\",\"Gorzów\":\"gorzow\",\"Hel\":\"hel\",\"Jelenia Góra\":\"jeleniagora\",\"Kalisz\":\"kalisz\",\"Kasprowy Wierch\":\"kasprowywierch\",\"Katowice\":\"katowice\",\"Kętrzyn\":\"ketrzyn\",\"Kielce\":\"kielce\",\"Kłodzko\":\"klodzko\",\"Koło\":\"kolo\",\"Kołobrzeg\":\"kolobrzeg\",\"Koszalin\":\"koszalin\",\"Kozienice\":\"kozienice\",\"Kraków\":\"krakow\",\"Krosno\":\"krosno\",\"Legnica\":\"legnica\",\"Lesko\":\"lesko\",\"Leszno\":\"leszno\",\"Lębork\":\"lebork\",\"Lublin\":\"lublin\",\"Łeba\":\"leba\",\"Łódź\":\"lodz\",\"Mikołajki\":\"mikolajki\",\"Mława\":\"mlawa\",\"Nowy Sącz\":\"nowysacz\",\"Olsztyn\":\"olsztyn\",\"Opole\":\"opole\",\"Ostrołęka\":\"ostroleka\",\"Piła\":\"pila\",\"Płock\":\"plock\",\"Poznań\":\"poznan\",\"Przemyśl\":\"przemysl\",\"Racibórz\":\"raciborz\",\"Resko\":\"resko\",\"Rzeszów\":\"rzeszow\",\"Sandomierz\":\"sandomierz\",\"Siedlce\":\"siedlce\",\"Słubice\":\"slubice\",\"Sulejów\":\"sulejow\",\"Suwałki\":\"suwalki\",\"Szczecin\":\"szczecin\",\"Szczecinek\":\"szczecinek\",\"Śnieżka\":\"sniezka\",\"Świnoujście\":\"swinoujscie\",\"Tarnów\":\"tarnow\",\"Terespol\":\"terespol\",\"Toruń\":\"torun\",\"Ustka\":\"ustka\",\"Warszawa\":\"warszawa\",\"Wieluń\":\"wielun\",\"Włodawa\":\"wlodawa\",\"Wrocław\":\"wroclaw\",\"Zakopane\":\"zakopane\",\"Zamość\":\"zamosc\",\"Zielona Góra\":\"zielonagora\"}");
-        List<String> cityList = new ArrayList<>(city.keySet());
+    public void initialize() throws IOException {
+        dg.getCities();
+        List<String> cityList = new ArrayList<>(dg.cities.keySet());
         cityList.sort(String::compareToIgnoreCase);
         cityChoice.getItems().addAll(cityList);
         }
